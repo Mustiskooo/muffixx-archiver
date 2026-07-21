c9c97f413e0f195939e97abeb28e812f437805c0
Mustiskooo <110821966+Mustiskooo@users.noreply.github.com>
2026-07-21 15:44:04 +0300

    cli

--- src/main.py
+++ src/main.py
@@ -1,22 +1,4 @@
-from cli import get_args
-from archiver import archive_site
-
-
-def main():
-    args = get_args()
-
-    ok, result = archive_site(
-        url=args.url,
-        output=args.output
-    )
-
-    if ok:
-        print(f"\n✅ Archive created successfully!")
-        print(f"📁 {result}")
-    else:
-        print(f"\n❌ Error:")
-        print(result)
-
+from cli import main
 
 if __name__ == "__main__":
     main()
